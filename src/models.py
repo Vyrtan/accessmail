4c4b4f02f5bde666d59383545deca40d4451466e
Your Name <you@example.com>
2014-06-04 18:57:10 +0200

    sqldb updates

--- src/models.py
+++ src/models.py
@@ -2,12 +2,34 @@ __author__ = 'ubuntu'
 
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class Email(Base):
-    __tablename__ = 'mail'
+class Mails(Base):
+    __tablename__ = 'mails'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), index=True)
+    date = Column(String(255))  #TODO: We should use the internal sqlite date type
+    subject = Column(String(255), index=True)
+    _from = Column(String(255))
+    to = Column(String(255))
+    cc = Column(Text)
+    bcc = Column(Text)
+    inReplyTo = Column(String(255), index=True)
+    messageId = Column(String(255), index=True)
+    inboxId = Column(Integer, ForeignKey('inboxes.id'))
+
+
+class Inbox(Base):
+    __tablename__ = 'inboxes'
+
+    id = Column(Integer, primary_key=True)
+    userMail = Column(String(255))  #TODO: We should use the internal sqlite date type
+    password = Column(String(255), index=True)
+    server = Column(String(255))
+    port = Column(String(255))
+    protocol = Column(String(255))
+    authProtocol = Column(String(255))
+    caches = relationship("ConCacheConcrete")
