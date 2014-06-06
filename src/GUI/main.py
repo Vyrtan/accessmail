285cd4464d8c5a350dd109722878b480a23e488e
grafgustav <grafgustav@rambler.ru>
2014-06-06 15:14:20 +0200

    added keyboard events, doesnt execute button methods yet though

--- src/GUI/main.py
+++ src/GUI/main.py
@@ -4,13 +4,9 @@ kivy.require('1.7.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-from kivy.clock import Clock
-from kivy.lang import Builder
+from kivy.properties import ObjectProperty, VariableListProperty
 
-
-#from kivy.core.window import Window
-
+from kivy.core.window import Window
 
 from src.GUI.addressLayout import AddressLayout
 from src.GUI.writeLayout import WriteLayout
@@ -24,21 +20,30 @@ class MenuLayout(BoxLayout):
 
 
 class Catalog(BoxLayout):
-    pass
 
+    def __init__(self, **kwargs):
+        super(Catalog, self).__init__(**kwargs)
+        screen_manager = ObjectProperty()
+        buttons = VariableListProperty()
+        self.current_butt = 0
+        Window.bind(on_key_down=self.rotate_buttons)
 
-# def rotate_buttons(keyboard, key,  *args):
-#     print("rotate buttons entered")
-#     # global current_butt
-#     if key == 9:
-#         current_butt += 1
-#         print("Nachher %s" %(current_butt))
-#     if key == 13:
-#         print("Return pressed")
-#         sm.current_screen.mn.buttons[current_butt].on_press()
-#     print("leaving rotate buttons")
-#     return
-# Window.bind(on_key_down=rotate_buttons)
+    def show_layout(self, value):
+        self.screen_manager.current = value
+        return
+
+    def rotate_buttons(self, keyboard, key,  *args):
+        print("enter rotate button")
+        print("current_butt: %d" %(self.current_butt))
+        if key == 9:
+            if self.current_butt < len(self.buttons)-1:
+                self.current_butt += 1
+            else:
+                self.current_butt = 0
+        if key == 13:
+            print("Return pressed")
+            self.buttons[self.current_butt].on_press()
+        print("leaving rotate buttons")
 
 
 class MainApp(App):
