5c0c065beffe24d18b1d3af651bf1e8ac581569d
grafgustav <grafgustav@rambler.ru>
2014-06-18 14:46:00 +0200

    updated testing protocol tried more stuff with ListView

--- src/GUI/overviewLayout.py
+++ src/GUI/overviewLayout.py
@@ -1,7 +1,9 @@
 __author__ = 'grafgustav'
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.adapters.dictadapter import DictAdapter
+from kivy.uix.listview import ListItemButton
 import CustomListViewItem
 
 
@@ -11,7 +13,25 @@ Builder.load_file('overviewLayout.kv')
 class OverviewLayout(Screen):
     list_view = ObjectProperty()
     label = ObjectProperty()
+    text = StringProperty()
 
-    def __init__(self):
-        super(OverviewLayout, self).__init__()
-        self.list_view.adapter.data = [1, 2, 3, 4]
+    def __init__(self, **kwargs):
+        super(OverviewLayout, self).__init__(kwargs)
+        list_item_args_converter = \
+                    lambda row_index, rec: {"name": rec["name"],
+                                            "address": rec["address"],
+                                            "subject": rec["subject"]}
+
+        sample = {"ob1":{"name": "Franz Bauer",
+                         "address": "franz.bauer@mail.ru",
+                         "subject": "empty"}}
+
+        my_adapter = DictAdapter(sorted_keys=sorted(sample.keys()),
+                                 data=sample,
+                                 args_converter=list_item_args_converter,
+                                 selection_mode="single",
+                                 allow_empty_selection=True,
+                                 cls=ListItemButton)
+
+        self.list_view.adapter = my_adapter
+        print "Listview initialized"
