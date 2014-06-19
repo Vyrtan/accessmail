18a0dec6b662370786ec48c5ef0ff53447ead769
Vyrtan <subway13>
2014-06-19 16:46:43 +0200

    added pop methods

--- src/Controller/CommunicationController.py
+++ src/Controller/CommunicationController.py
@@ -3,11 +3,8 @@ import MainController
 import smtplib
 import email #TODO: Python mail class im Modell?
 import imaplib
+import poplib
 
-def _imap_login(self):
-    connection = imaplib.IMAP4_SSL() #TODO: Daten aus Model laden, SSL ?
-    connection.login()
-    return connection
 
 class CommunicationController(object):
 
@@ -15,6 +12,16 @@ class CommunicationController(object):
         self.__credentials = self._load_credentials()
         self.__inbox = self._load_inbox()
         self.__main_controller = main_controller
+
+    def _imap_login(self):
+        connection = imaplib.IMAP4_SSL() #TODO: Daten aus Model laden, SSL ?
+        connection.login()
+        return connection
+
+    def _pop_login(self):
+        connection = poplib.POP3_SSL()
+        connection.login()
+        return connection
 
     def _load_credentials(self):
         cred = self.__main_controller.get_database_controller().load_credentials()
@@ -35,15 +42,15 @@ class CommunicationController(object):
 
 
 
-    def _getmailboxes(self):
-        c = _imap_login()
+    def _getmailboxes_imap(self):
+        c = self._imap_login()
         typ, data = c.list()    #TODO: Richtige Mailbox auswaehlen(erstmal nur "Bekannte?")
         c.close()
         c.logout()
         return data
 
-    def _getmails(self):
-        c = _imap_login()
+    def _getmails_imap(self):
+        c = self._imap_login()
         c.select()
         rv, data = c.search(None, "ALL")           #TODO: Mailbox auswaehlen, holt sich erstmal ALLE mails
         for num in data[0].split():
@@ -53,4 +60,9 @@ class CommunicationController(object):
         c.logout()
         return msg         #TODO: Daten weiter verarbeiten
 
-    # TODO: delete
+    def _getmails_pop(self):
+        c = self._pop_login()
+        msg = c.list()      #TODO: alle Messages in String format (liste)
+        c.quit()
+
+    # TODO: delete imap/pop
