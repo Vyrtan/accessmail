acc29fa8878ac4ce17ef2046e2045dd1d80fd5b5
Vyrtan <subway13>
2014-06-24 13:23:21 +0200

    delete popup in contacts

--- src/GUI/ContactItem.py
+++ src/GUI/ContactItem.py
@@ -2,10 +2,12 @@ __author__ = 'grafgustav'
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.popup import Popup
 from src.Controller.DatabaseController import DatabaseController
 
 
 Builder.load_file("GUI/ContactItem.kv")
+Builder.load_file("GUI/deletePopupContact.kv")
 
 
 class ContactItem(BoxLayout):
@@ -21,8 +23,21 @@ class ContactItem(BoxLayout):
         self.email = contact.emailAddress
 
     def trigger_delete(self):
-       print self.root.parent.parent.parent.deleteContact(self.contact)
+       d = self.root
+       p = DeletePopupContacts(self.contact, d)
+       p.open()
+
 
     # switches to WriteLayout and already fills in the address (supposedly)
     def trigger_write_mail(self):
         self.root.parent.parent.parent.parent.parent.parent.show_layout("Write", address=self.email)
+
+class DeletePopupContacts(Popup):
+
+    def __init__(self, contact, d):
+        super(DeletePopupContacts, self).__init__()
+        self.cont = contact
+        self.chef = d
+
+    def deleteContact(self):
+       self.chef.parent.parent.parent.deleteContact(self.cont)
