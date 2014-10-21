850dfd6610114fdbaa24a25d77174e165c0a2253
grafgustav <grafgustav@rambler.ru>
2014-10-21 15:13:41 +0200

    added Tooltips in the menu bar

---
+++ src/GUI/Tooltip.py
@@ -0,0 +1,36 @@
+from kivy.core.window import Window
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.input.providers.mouse import Color
+from kivy.properties import StringProperty, ListProperty, BooleanProperty, NumericProperty
+from kivy.uix.label import Label
+
+__author__ = 'grafgustav'
+
+
+class ToolTip(Label):
+    #default properties
+    #text property, default property value "Click to edit"
+    text = StringProperty("Click to edit")
+    #text color as list, rgba value default black with 100% opacity
+    color = ListProperty([0,0,0,0.2])
+    bold = BooleanProperty(True)
+    font_size = NumericProperty(20)
+    #background colour as an rgb list,value default yellow
+    background = ListProperty([1,1,1,0])
+
+    #initialise tooltip label
+    def __init__(self, **kwargs):
+        super(ToolTip, self).__init__(**kwargs)
+        # set position at current mouse cursor position
+        self.pos = (Window.mouse_pos[0], Window.mouse_pos[1])
+        # update the texture value of the widget
+        self.texture_update()
+        # set the size of the label to that of the text
+        self.size = self.texture_size
+        # draw the label background
+        with self.canvas.before:
+            #set the label background color to the background property as rgb list color
+            #sure there must be a better method than this but it works
+            # self.color = Color(self.background[0], self.background[1], self.background[2])
+            #draw in the background rectangle
+            self.bg_rect = Rectangle(pos=self.pos, size=self.size)
--- src/GUI/menuLayout.py
+++ src/GUI/menuLayout.py
@@ -1,10 +1,92 @@
 __author__ = 'grafgustav'
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from Tooltip import ToolTip
 
 
 Builder.load_file('GUI/menuLayout.kv')
 
 
 class MenuLayout(Screen):
-    pass
+
+    inboxBtn = ObjectProperty()
+    outboxBtn = ObjectProperty()
+    writeBtn = ObjectProperty()
+    contactBtn = ObjectProperty()
+    exitBtn = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(MenuLayout, self).__init__(**kwargs)
+        Clock.schedule_interval(self.check_mouse_pos, 1)
+        self.tooltip_active = ""
+
+    def check_mouse_pos(self, _):
+        pos = Window.mouse_pos
+
+        if self.inboxBtn.collide_point(pos[0], pos[1]):
+            if (not self.tooltip_active) | (self.tooltip_active != "inbox"):
+                tp = ToolTip(text="Inbox")
+                self.inboxBtn.add_widget(tp)
+                self.tooltip_active = "inbox"
+                # clear all other tooltips
+                self.contactBtn.clear_widgets()
+                self.outboxBtn.clear_widgets()
+                self.exitBtn.clear_widgets()
+                self.writeBtn.clear_widgets()
+
+        elif self.outboxBtn.collide_point(pos[0], pos[1]):
+            if (not self.tooltip_active) | (self.tooltip_active != "outbox"):
+                tp = ToolTip(text="Sent\nMessages")
+                self.outboxBtn.add_widget(tp)
+                self.tooltip_active = "outbox"
+                # clear all other tooltips
+                self.inboxBtn.clear_widgets()
+                self.contactBtn.clear_widgets()
+                self.exitBtn.clear_widgets()
+                self.writeBtn.clear_widgets()
+
+        elif self.writeBtn.collide_point(pos[0], pos[1]):
+            if (not self.tooltip_active) | (self.tooltip_active != "write"):
+                tp = ToolTip(text="Write a\nnew E-mail")
+                self.writeBtn.add_widget(tp)
+                self.tooltip_active = "write"
+                # clear all other tooltips
+                self.inboxBtn.clear_widgets()
+                self.outboxBtn.clear_widgets()
+                self.exitBtn.clear_widgets()
+                self.contactBtn.clear_widgets()
+
+        elif self.contactBtn.collide_point(pos[0], pos[1]):
+            if (not self.tooltip_active) | (self.tooltip_active != "contact"):
+                tp = ToolTip(text="Add and delete\nContacts")
+                self.contactBtn.add_widget(tp)
+                self.tooltip_active = "contact"
+                # clear all other tooltips
+                self.inboxBtn.clear_widgets()
+                self.outboxBtn.clear_widgets()
+                self.exitBtn.clear_widgets()
+                self.writeBtn.clear_widgets()
+        elif self.exitBtn.collide_point(pos[0], pos[1]):
+            if (not self.tooltip_active) | (self.tooltip_active != "exit"):
+                tp = ToolTip(text="Exit the\nprogram")
+                self.exitBtn.add_widget(tp)
+                self.tooltip_active = "exit"
+                # clear all other tooltips
+                self.inboxBtn.clear_widgets()
+                self.outboxBtn.clear_widgets()
+                self.contactBtn.clear_widgets()
+                self.writeBtn.clear_widgets()
+        else:
+            self.tooltip_active = ""
+            self.inboxBtn.clear_widgets()
+            self.outboxBtn.clear_widgets()
+            self.contactBtn.clear_widgets()
+            self.writeBtn.clear_widgets()
+            self.exitBtn.clear_widgets()
+
+
+
+
